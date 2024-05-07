45232091345ab0f36a31eb4df5bb57e5268b3d6b
PabloSnhz <pablosanhuezaaa@gmail.com>
2024-05-06 21:04:58 -0300

    sheetslogica

--- src/app/components/pages/niveles-matematica/niveles-matematica.component.ts
+++ src/app/components/pages/niveles-matematica/niveles-matematica.component.ts
@@ -8,130 +8,84 @@ import { SheetsDatesService } from 'src/app/services/sheets-dates.service';
 })
 export class NivelesMatematicaComponent {
 
-//   questionsData: any[] = [];
-//   currentQuestionIndex = 0;
-//   totalQuestionsToShow = 15;
-//   currentLevel = 1;
+datos: any[] = [];
 
-  constructor(private sheetsService: SheetsDatesService) {}
+
+selectedOption: string | null = null;
+  questionsData: any[] = [];
+  currentQuestionIndex: number | null = null;
+  modalIds: string[] = [];
+  correctAnswerIndices: Set<number> = new Set<number>();
+
+  constructor(private sheetsService: SheetsDatesService) { }
 
   ngOnInit(): void {
-    this.sheetsService.getSheets().subscribe((data: any) => {
-      // Aquí puedes hacer lo que quieras con los datos obtenidos del CSV
-      console.log('Datos del CSV:', data);
-    })
+    this.sheetsService.getSheets().subscribe(data => {
+      this.questionsData = data;
+      this.modalIds = data.map((_, index) => `static${index}`);
+      // Inicializamos currentQuestionIndex al primer índice
+      this.currentQuestionIndex = 0;
+
+      this.iniciarTemporizador()
+    });
   }
-//   ngOnInit(): void {
-//     this.loadQuestions(this.currentLevel);
-//   }
 
-//   loadQuestions(nivel: number) {
-//     this.sheetsService.getSheets().subscribe(
-//       (contenido: any[]) => {
-//         this.questionsData = contenido.filter(item => item.nivel === nivel);
-//         this.questionsData = this.questionsData.slice(0, this.totalQuestionsToShow);
-//         this.showQuestion(this.currentQuestionIndex);
-//       },
-//       error => {
-//         console.error('Error fetching questions data:', error);
-//       }
-//     );
-//   }
+  prevQuestion() {
+    if (this.currentQuestionIndex !== null && this.currentQuestionIndex > 0) {
+      this.currentQuestionIndex--;
+    }
+  }
 
-//   showQuestion(index: number) {
-//     const pregunta = this.questionsData[index];
-//     const storedResponse = localStorage.getItem(`response_${this.currentLevel}_${index}`);
-//     const storedCorrect = localStorage.getItem(`correct_${this.currentLevel}_${index}`);
+  nextQuestion() {
+    if (this.currentQuestionIndex !== null && this.currentQuestionIndex < this.questionsData.length - 1) {
+      this.currentQuestionIndex++;
+    }
+  }
 
-//     // Crear el elemento HTML para mostrar la pregunta y sus opciones
-//     const questionDiv = document.createElement('div');
-//     questionDiv.classList.add('container', 'mt-sm-5', 'my-1');
+  isAnswerCorrect(index: number, selectedOption: string | null): boolean {
+    if (selectedOption === null) {
+      return false; // Si no se ha seleccionado ninguna opción, consideramos la respuesta incorrecta.
+    }
+    const pregunta = this.questionsData[index];
+    return pregunta.respuestas === selectedOption;
+  }
 
-//     questionDiv.innerHTML = `
-//       <div class="question ml-sm-5 pl-sm-5 pt-2">
-//           <div class="py-2 h5 mb-4"><b>Pregunta ${pregunta.pregunta}: ${pregunta.enunciado}</b></div>
-//           <div class="ml-md-3 ml-sm-3 pl-md-5 pt-sm-0 pt-3" id="options${index}">
-//               <div class="option mb-3">
-//                   <input type="radio" id="option_a" name="options${index}" value="a" ${(storedResponse === 'a') ? 'checked' : ''} ${storedResponse ? 'disabled' : ''}>
-//                   <label for="option_a">A) ${pregunta.opcion_a}</label>
-//               </div>
-//               <div class="option mb-3">
-//                   <input type="radio" id="option_b" name="options${index}" value="b" ${(storedResponse === 'b') ? 'checked' : ''} ${storedResponse ? 'disabled' : ''}>
-//                   <label for="option_b">B) ${pregunta.opcion_b}</label>
-//               </div>
-//               <div class="option mb-3">
-//                   <input type="radio" id="option_c" name="options${index}" value="c" ${(storedResponse === 'c') ? 'checked' : ''} ${storedResponse ? 'disabled' : ''}>
-//                   <label for="option_c">C) ${pregunta.opcion_c}</label>
-//               </div>
-//               <div class="option mb-3">
-//                   <input type="radio" id="option_d" name="options${index}" value="d" ${(storedResponse === 'd') ? 'checked' : ''} ${storedResponse ? 'disabled' : ''}>
-//                   <label for="option_d">D) ${pregunta.opcion_d}</label>
-//               </div>
-//           </div>
-//           <div class="alert alert-success alert-dismissible fade show text-center ${storedCorrect === 'true' ? '' : 'd-none'}" id="alertCorrect${index}" role="alert">
-//               <strong>¡Respuesta correcta!</strong>
-//               <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-//           </div>
-
-//           <div class="alert alert-danger alert-dismissible fade show text-center ${storedCorrect === 'false' ? '' : 'd-none'}" id="alertIncorrect${index}" role="alert">
-//               <strong>La respuesta correcta era: ${pregunta.respuestas}</strong>
-//               <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-//               <a href="#" class="bg-transparent border-0 justification-link" data-bs-toggle="modal" data-bs-target="#static${index}">
-//                   <strong>Conocé la justificación. Haz Click aquí.</strong>
-//               </a>
-//           </div>
-
-//           <!-- Modal -->
-//           <div class="modal fade" id="static${index}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-//               <div class="modal-dialog modal-dialog-centered">
-//                   <div class="modal-content">
-//                       <div class="modal-body">
-//                           ${pregunta.explicacion}
-//                       </div>
-//                   <div class="modal-footer">
-//                       <button type="button" class="btn btn-gray border border-dark text-black" data-bs-dismiss="modal">Ok! Gracias.</button>
-//                   </div>
-//               </div>
-//           </div>
-//           </div>
-//       </div>
-//   `;
-
-
-// }
-// }
-  // data: any[] = [];
-  // imagesLoaded: boolean = false;
-
-  // constructor(private sheetsDatesService: SheetsDatesService) { }
-
-  // ngOnInit(): void {
-  //   this.sheetsDatesService.getSheets().subscribe(
-  //     (data) => {
-  //       this.data = data;
-  //     },
-  //     (error) => {
-  //     console.error('Error fetching sheets data:', error);
-  //     }
-  //   );
-  // }
-
-
-
-
-  // getImages() {
-  //   this.sheetsDatesService.getSheets().subscribe(
-  //     (data) => {
-  //       this.imagesLoaded = true;
-  //       this.data = data;
-  //       console.log(data)
-  //     },
-  //     (error) => {
-  //       console.error('Error fetching dates:', error);
-  //     }
-  //   );
-  // }
+iniciarTemporizador() {
+    let segundos = 0;
+    setInterval(() => {
+        segundos++;
+        const horas = Math.floor(segundos / 3600);
+        const minutos = Math.floor((segundos % 3600) / 60);
+        const segundosRestantes = segundos % 60;
+        const temporizadorElement = document.getElementById('temporizador');
+        if (temporizadorElement) {
+            temporizadorElement.innerText = `${horas < 10 ? '0' : ''}${horas}:${minutos < 10 ? '0' : ''}${minutos}:${segundosRestantes < 10 ? '0' : ''}${segundosRestantes}`;
+        }
+    }, 1000);
+}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // ngOnInit(): void {
+  //   this.sheetsService.getSheets().subscribe((data: any) => {
+  //     this.datos = data;
+  //     console.log('Datos del CSV:', data);
+  //   })
+  // }
+
 
 
--- src/app/services/sheets-dates.service.ts
+++ src/app/services/sheets-dates.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, map, catchError, throwError } from 'rxjs';
+import { Observable, map, catchError } from 'rxjs';
+import { parse } from 'papaparse';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -9,48 +11,90 @@ export class SheetsDatesService {
   constructor(private httpClient: HttpClient) {}
 
   getSheets(): Observable<any[]> {
-    return this.httpClient.get('https://docs.google.com/spreadsheets/d/e/2PACX-1vRgkcYFvN0hy7HemFKxjnV3TiRzX15UE0BE5VyBetsdm87fOo1U76UsGIrNUPA-7P3eKD_n_Iqsmcrv/pub?output=csv', { responseType: 'text' })
-      .pipe(
-        map((response: string) => {
-          try {
-            const rows = response.split('\n');
-            const contenido = [];
-            for (let i = 1; i < rows.length; i++) {
-              const rowData = rows[i].split(',').map(cell => cell.trim().replace(/^\"|\"$/g, ''));
-              const rowObj: any = {};
-              if (rowData.length >= 13) {
-                rowObj.pregunta = rowData[0] || '';
-                rowObj.enunciado = rowData[1] || '';
-                rowObj.a = rowData[2] || '';
-                rowObj.b = rowData[3] || '';
-                rowObj.c = rowData[4] || '';
-                rowObj.d = rowData[5] || '';
-                rowObj.respuestas = rowData[6] ? rowData[6].replace(/^\"|\"$/g, '') : '';
-                rowObj.explicacion = rowData[7] || '';
-                rowObj.tema = rowData[8] || '';
-                rowObj.contenido = rowData[9] || '';
-                rowObj.imagen = rowData[10] || '';
-
-
-                rowObj.contenido += ', ' + rowData.slice(11, -1).join(',').replace(/^\"|\"$/g, '');
-
-                contenido.push(rowObj);
-              }
-            }
-            localStorage.setItem('datosSheets', JSON.stringify(contenido));
-            return contenido;
-          } catch (error) {
-            console.error('Error processing data:', error);
-            return [];
-          }
-        }),
-        catchError((error) => {
-          console.error('Error fetching data:', error);
-          return throwError(error);
-        })
-      );
+    return this.httpClient.get('https://docs.google.com/spreadsheets/d/e/2PACX-1vRgkcYFvN0hy7HemFKxjnV3TiRzX15UE0BE5VyBetsdm87fOo1U76UsGIrNUPA-7P3eKD_n_Iqsmcrv/pub?output=csv', { responseType: 'text' }).pipe(
+      map((csvData: string) => {
+        const jsonData = parse(csvData, { header: true });
+        const data: any[] = jsonData.data.map((row: any) => {
+          return {
+            pregunta: row.pregunta,
+            enunciado: row.enunciado,
+            a: row.a,
+            b: row.b,
+            c: row.c,
+            d: row.d,
+            respuestas: row.respuestas,
+            explicacion: row.explicacion,
+            titulo: row.titulo,
+            teoria: row.teoria,
+            imagen: row.imagen
+          };
+        });
+        localStorage.setItem('datosSheets', JSON.stringify(data));
+        return data;
+      }),
+      catchError((error) => {
+        console.error('Error fetching data:', error);
+        return [];
+      })
+    );
   }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   getSheets(): Observable<any[]> {
+//     return this.httpClient.get('https://docs.google.com/spreadsheets/d/e/2PACX-1vRgkcYFvN0hy7HemFKxjnV3TiRzX15UE0BE5VyBetsdm87fOo1U76UsGIrNUPA-7P3eKD_n_Iqsmcrv/pub?output=csv', { responseType: 'text' })
+//       .pipe(
+//         map((response: string) => {
+//           try {
+//             const rows = response.split('\n');
+//             const contenido = [];
+//             for (let i = 1; i < rows.length; i++) {
+//               const rowData = rows[i].split(',');
+//               const rowObj: any = {};
+//               if (rowData.length >= 13) {
+//                 rowObj.pregunta = rowData[0].trim().replace(/^"|"$/g, '') || '';
+//                 rowObj.enunciado = rowData[1].trim().replace(/^"|"$/g, '') || '';
+//                 rowObj.a = rowData[2].trim().replace(/^"|"$/g, '') || '';
+//                 rowObj.b = rowData[3].trim().replace(/^"|"$/g, '') || '';
+//                 rowObj.c = rowData[4].trim().replace(/^"|"$/g, '') || '';
+//                 rowObj.d = rowData[5].trim().replace(/^"|"$/g, '') || '';
+//                 rowObj.respuestas = rowData[6].trim().replace(/^"|"$/g, '') || '';
+//                 rowObj.explicacion = rowData[7].trim().replace(/^"|"$/g, '') || '';
+//                 rowObj.tema = rowData[8].trim().replace(/^"|"$/g, '') || '';
+//                 rowObj.contenido = rowData[9].trim().replace(/^"|"$/g, '') || '';
+//                 rowObj.imagen = rowData[10].trim().replace(/^"|"$/g, '') || '';
+//                 rowObj.contenido += ', ' + rowData.slice(11).map(cell => cell.trim().replace(/^"|"$/g, '')).join(',');
+//                 contenido.push(rowObj);
+//               }
+//             }
+//             localStorage.setItem('datosSheets', JSON.stringify(contenido));
+//             return contenido;
+//           } catch (error) {
+//             console.error('Error processing data:', error);
+//             return [];
+//           }
+//         }),
+//         catchError((error) => {
+//           console.error('Error fetching data:', error);
+//           return throwError(error);
+//         })
+//       );
+//   }
+
+// }
 
 
