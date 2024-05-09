8223f4af29b93482981ec7432a8f6d1be86ccc55
PabloSnhz <pablosanhuezaaa@gmail.com>
2024-05-09 18:24:25 -0300

    feedbackChoice

--- src/app/components/pages/razonamientoLogico/niveles-matematica/niveles-matematica.component.ts
+++ src/app/components/pages/razonamientoLogico/niveles-matematica/niveles-matematica.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SheetsDatesService } from 'src/app/services/sheets-dates.service';
-
 
 @Component({
   selector: 'app-niveles-matematica',
@@ -9,118 +8,105 @@ import { SheetsDatesService } from 'src/app/services/sheets-dates.service';
 })
 export class NivelesMatematicaComponent implements OnInit {
 
-datos: any[] = [];
-
+  datos: any[] = [];
   selectedOption: string | null = null;
   questionsData: any[] = [];
   levels: any[] = [];
   currentLevelIndex: number = 0;
-  currentQuestionIndex: number | null = null;
+  currentQuestionIndex: number = 0;
   modalIds: string[] = [];
-  selectedOptions: (string | null)[] = [];
+  selectedOptions: (string | null)[][] = [];
+  answeredQuestionsCounts: number[] = [];
 
-constructor(private sheetsService: SheetsDatesService) { }
+  constructor(private sheetsService: SheetsDatesService) { }
 
+  ngOnInit(): void {
+    this.sheetsService.getSheets().subscribe(data => {
+      this.questionsData = data;
+      const numQuestionsPerLevel = 5;
+      for (let i = 0; i < this.questionsData.length; i += numQuestionsPerLevel) {
+        this.levels.push(this.questionsData.slice(i, i + numQuestionsPerLevel));
+        this.answeredQuestionsCounts.push(0);
+      }
+      this.selectedOptions = this.levels.map(() => new Array(5).fill(null));
+      this.modalIds = this.questionsData.map((_, index) => 'modal_' + index);
+      this.iniciarTemporizador();
+    });
+  }
 
-ngOnInit(): void {
-  this.sheetsService.getSheets().subscribe(data => {
-    this.questionsData = data;
-    const numQuestionsPerLevel = 5;
-    for (let i = 0; i < this.questionsData.length; i += numQuestionsPerLevel) {
-      this.levels.push(this.questionsData.slice(i, i + numQuestionsPerLevel));
+  prevLevel() {
+    if (this.currentLevelIndex > 0) {
+      this.currentLevelIndex--;
+      this.currentQuestionIndex = 0;
     }
-    this.selectedOptions = new Array(this.questionsData.length).fill(null);
-    this.currentQuestionIndex = 0;
-
-    this.iniciarTemporizador();
-  });
-}
-
-prevLevel() {
-  if (this.currentLevelIndex > 0) {
-    this.currentLevelIndex--;
-    this.currentQuestionIndex = 0;
   }
-}
 
-nextLevel() {
-  if (this.currentLevelIndex < this.levels.length - 1) {
-    this.currentLevelIndex++;
-    this.currentQuestionIndex = 0;
-  }
-}
-
-selectLevel(levelIndex: number) {
-  this.currentLevelIndex = levelIndex;
-  this.currentQuestionIndex = 0;
-}
-
-
-prevQuestion() {
-  if (this.currentQuestionIndex !== null && this.currentQuestionIndex > 0) {
-    this.currentQuestionIndex--;
-  }
-}
-
-nextQuestion() {
-  if (this.currentQuestionIndex !== null && this.currentQuestionIndex < this.levels[this.currentLevelIndex].length - 1) {
-    this.currentQuestionIndex++;
-  }
-}
-
-isAnswerCorrect(index: number): boolean {
-  const pregunta = this.questionsData[index];
-  const selectedOption = this.selectedOptions[index];
-  if (selectedOption === null) {
-    return false;
-  }
-  return pregunta.respuestas === pregunta[selectedOption];
-}
-
-
-
-onOptionSelected(index: number, opcion: string) {
-  this.selectedOptions[index] = opcion;
-  this.selectedOption = opcion;
-  this.levels[this.currentLevelIndex][index].answered = true;
-}
-
-iniciarTemporizador() {
-  let segundos = 0;
-  setInterval(() => {
-    segundos++;
-    const horas = Math.floor(segundos / 3600);
-    const minutos = Math.floor((segundos % 3600) / 60);
-    const segundosRestantes = segundos % 60;
-    const temporizadorElement = document.getElementById('temporizador');
-    if (temporizadorElement) {
-      temporizadorElement.innerText = `${horas < 10 ? '0' : ''}${horas}:${minutos < 10 ? '0' : ''}${minutos}:${segundosRestantes < 10 ? '0' : ''}${segundosRestantes}`;
+  nextLevel() {
+    if (this.currentLevelIndex < this.levels.length - 1) {
+      this.currentLevelIndex++;
+      this.currentQuestionIndex = 0;
     }
-  }, 1000);
+  }
+
+  selectLevel(levelIndex: number) {
+    this.currentLevelIndex = levelIndex;
+    this.currentQuestionIndex = 0;
+  }
+
+  prevQuestion() {
+    if (this.currentQuestionIndex > 0) {
+      this.currentQuestionIndex--;
+    }
+  }
+
+  nextQuestion() {
+    if (this.currentQuestionIndex < this.levels[this.currentLevelIndex].length - 1) {
+      this.currentQuestionIndex++;
+    } else {
+      this.answeredQuestionsCounts[this.currentLevelIndex]++;
+      if (this.answeredQuestionsCounts[this.currentLevelIndex] === 5) {
+        document.getElementById('resetButton' + this.currentLevelIndex)!.removeAttribute('hidden');
+      }
+    }
+  }
+
+  isAnswerCorrect(levelIndex: number, questionIndex: number): boolean {
+    const pregunta = this.questionsData[levelIndex * 5 + questionIndex];
+    const selectedOption = this.selectedOptions[levelIndex][questionIndex];
+    if (selectedOption === null) {
+      return false;
+    }
+    return pregunta.respuestas === pregunta[selectedOption];
+  }
+
+  onOptionSelected(levelIndex: number, questionIndex: number, option: string) {
+    this.selectedOptions[levelIndex][questionIndex] = option;
+    this.levels[levelIndex][questionIndex].answered = true;
+  }
+
+  iniciarTemporizador() {
+    let segundos = 0;
+    setInterval(() => {
+      segundos++;
+      const horas = Math.floor(segundos / 3600);
+      const minutos = Math.floor((segundos % 3600) / 60);
+      const segundosRestantes = segundos % 60;
+      const temporizadorElement = document.getElementById('temporizador');
+      if (temporizadorElement) {
+        temporizadorElement.innerText = `${horas < 10 ? '0' : ''}${horas}:${minutos < 10 ? '0' : ''}${minutos}:${segundosRestantes < 10 ? '0' : ''}${segundosRestantes}`;
+      }
+    }, 1000);
+  }
+
+  resetQuestions(levelIndex: number) {
+    this.selectedOptions[levelIndex] = new Array(5).fill(null);
+    this.answeredQuestionsCounts[levelIndex] = 0;
+
+    const options = document.querySelectorAll('input[name="options' + levelIndex + '"]');
+    options.forEach((option: any) => {
+      option.checked = false;
+    });
+
+    document.getElementById('resetButton' + levelIndex)!.setAttribute('hidden', 'true');
+  }
 }
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // ngOnInit(): void {
-  //   this.sheetsService.getSheets().subscribe((data: any) => {
-  //     this.datos = data;
-  //     console.log('Datos del CSV:', data);
-  //   })
-  // }
-
-
-
